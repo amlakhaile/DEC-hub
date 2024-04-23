4fe14bd18beb452e279edb2752a5d6f5936a4c98
haileamlak <amlakhaile@gmail.com>
2024-04-23 22:09:22 +0300

    Login page is done

--- eduhub/src/pages/Login.jsx
+++ eduhub/src/pages/Login.jsx
@@ -6,11 +6,12 @@ const Login = () => {
     <div className='flex h-screen items-center justify-evenly '>
     <div className='px-11 mx-11 mb-11 pb-11'>
       <img src={logo} alt="Logo" className='w-[60%]'/>
-      <h1 className='pl-[160px] text-[20px] font-bold'>DEC HUB</h1>
+     
     </div>
     <div>
     <form className="bg-white w-[400px] pr-11 pt-6 pb-8 mb-4 mr-11">
         <div className="mb-4">
+        <h1 className='pl-[135px] text-[20px] font-bold pb-7  text-gray-700'>DEC HUB</h1>
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
             Email
           </label>
@@ -32,13 +33,19 @@ const Login = () => {
             placeholder="Password"
           />
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex justify-center">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="mb-5 mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
           >
             Sign In
           </button>
+        </div>
+        <div className="text-center">
+          <span className="mr-1">Don't have an account?</span>
+          <a href="#" className="text-blue-500 hover:underline">
+            Create New Account
+          </a>
         </div>
       </form>
     </div>
